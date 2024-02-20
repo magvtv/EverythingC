d8bd5ea5b9723fda77feb0394ce3f1ace00f2b9b
magvtv <magutujr@gmail.com>
2024-02-20 09:05:57 +0300

    done with making Q1 into glut

--- C++/Graphics/CAT2/Q1: Circle Drawing/Bresenham/bresenham.cpp
+++ C++/Graphics/CAT2/Q1: Circle Drawing/Bresenham/bresenham.cpp
@@ -1,67 +1,78 @@
-#include <GLFW/glfw3.h>
+#include <GL/glut.h>
 #include <iostream>
-#include <vector>
+#include <cmath>
 
-GLFWwindow *window;
-
-void drawSymmetricPoints(int x, int y, int centerX, int centerY)
+void drawSymmetricPoints(int x, int y, int cx, int cy)
 {
     glBegin(GL_POINTS);
-    glVertex2i((x + centerX), (y + centerY));
-    glVertex2i((-x + centerX), (y + centerY));
-    glVertex2i((x + centerX), (-y + centerY));
-    glVertex2i((-x + centerX), (-y + centerY));
+    glVertex2i((cx + x), (cy + y));
+    glVertex2i((cx - x), (cy + y));
+    glVertex2i((cx + x), (cy - y));
+    glVertex2i((cx - x), (cy - y));
     glEnd();
 }
 
-void BresenhamCircle(int centerX, int centerY, int radius)
+// implementing the bresenham's circle algorithm
+void BresenhamCircle(int cx, int cy, int radius)
 {
     int x = 0;
     int y = radius;
     int d = 3 - (2 * radius);
 
-    drawSymmetricPoints(x, y, centerX, centerY);
+    drawSymmetricPoints(x, y, cx, cy);
 
     while (x <= y)
     {
         if (d <= 0)
         {
-            d = d + (4 * x) + 6;
+            d += (4 * x) + 6;
         }
         else
         {
-            d = d + (4 * (x - y)) + 10;
+            d += (4 * (x - y)) + 10;
             y--;
         }
         x++;
-        drawSymmetricPoints(x, y, centerX, centerY);
+        drawSymmetricPoints(x, y, cx, cy);
     }
 }
-
-void initOpenGL()
+// function that draws the black diameter
+void drawLine(int x1, int y1, int x2, int y2)
 {
-    glfwInit();
-    window = glfwCreateWindow(800, 600, "Bresenham\'s Red Circle", NULL, NULL);
-    glfwMakeContextCurrent(window);
+    glColor3f(0.0f, 0.0f, 0.0f);
+    glBegin(GL_LINES);
+    glVertex2i(x1, y1);
+    glVertex2i(x2, y2);
+    glEnd();
 }
 
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
     glColor3f(1.0f, 0.0f, 0.0f);
-    BresenhamCircle(300, 300, 400);
+    BresenhamCircle(3, 3, 4);
+    drawLine((3 - 4), 3, (3 + 4), 3);
     glFlush();
 }
 
-int main () {
+void initOpenGL()
+{
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0, 6, 0, 6);
+}
+
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(800, 600);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Bresenham\'s Circle & Black Diameter");
+
     initOpenGL();
-
-    while (!glfwWindowShouldClose(window)) {
-        display();
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
+    glutDisplayFunc(display);
+    glutMainLoop();
     return 0;
 }
