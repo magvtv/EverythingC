6c1bfa6dfdb94ca9ff41a07b39159269774e87ae
magvtv <magutujr@gmail.com>
2024-02-20 08:45:25 +0300

    done the circle algorithm with glut library

--- C++/Graphics/CAT2/Q1: Circle Drawing/Midpoint/midpoint.cpp
+++ C++/Graphics/CAT2/Q1: Circle Drawing/Midpoint/midpoint.cpp
@@ -1,24 +1,24 @@
+#include <GL/glut.h>
 #include <iostream>
-#include <vector>
-void drawSymmetricalPoints(int x, int y, int centerX, int centerY)
+#include <cmath>
+void drawSymmetricalPoints(int x, int y)
 {
-    // draw a given point with x, y cartesian 1st octant
-    std::cout << "(" << (x + centerX) << ", " << (y + centerY) << ")\n";
-    // draw given point with x, y in 2nd octant
-    std::cout << "(" << (-x + centerX) << ", " << (y + centerY) << ")\n";
-    // draw given point with x, y in 3rd octant
-    std::cout << "(" << (x + centerX) << ", " << (-y + centerY) << ")\n";
-    // draw given point with x, y in 4th octant
-    std::cout << "(" << (-x + centerX) << ", " << (-y + centerY) << ")\n";
+    glBegin(GL_POINTS);
+    glVertex2i(x, y);
+    glVertex2i(-x, y);
+    glVertex2i(x, -y);
+    glVertex2i(-x, -y);
+    glEnd();
 }
 
-void MidPointCircle(int centerX, int centerY, int radius)
+// implementing the midpoint circle algorithm with (cx, cy) c for 'center'
+void MidPointCircle(int cx, int cy, int radius)
 {
     int x = 0;
     int y = radius;
     int p = 1 - radius;
 
-    drawSymmetricalPoints(x, y, centerX, centerY);
+    drawSymmetricalPoints(cx, cy);
 
     while (x < y)
     {
@@ -34,18 +34,47 @@ void MidPointCircle(int centerX, int centerY, int radius)
             p += (2 * (x - y)) + 1;
         }
 
-        drawSymmetricalPoints(x, y, centerX, centerY);
+        drawSymmetricalPoints((cx + x), (cy + y));
+        drawSymmetricalPoints((cx - x), (cy + y));
+        drawSymmetricalPoints((cx + x), (cy - y));
+        drawSymmetricalPoints((cx - x), (cy - y));
+
+        drawSymmetricalPoints((cx + y), (cy + x));
+        drawSymmetricalPoints((cx - y), (cy + x));
+        drawSymmetricalPoints((cx + y), (cy - x));
+        drawSymmetricalPoints((cx - y), (cy - x));
     }
 }
 
-int main()
+// rendering the actual grpahics
+void display()
 {
-    int centerX = 3;
-    int centerY = 3;
-    int radius = 4;
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    MidPointCircle(3, 3, 4);
+    glFlush();
+}
 
-    std::cout << "Points along circumference of the circle: \n";
-    MidPointCircle(centerX, centerY, radius);
+// initializing opengl settings
+void init()
+{
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f); // white background
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0, 6, 0, 6); // setting the coordinate system
+}
+
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(800, 600);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Midpoint Circle Algorithm");
+
+    init();
+    glutDisplayFunc(display);
+    glutMainLoop();
 
     return 0;
 }
