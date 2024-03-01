8edae06148cc036eea44b7c9f2d32787db6ffab5
magvtv <magutujr@gmail.com>
2024-03-01 09:09:01 +0300

    trying to merge with branch main

---
+++ C++/Graphics/Blender Sierpinski/sierpinski.cpp
@@ -0,0 +1,73 @@
+#include <GL/glut.h>
+#include <iostream>
+#include <math.h>
+#include <vector>
+
+using namespace std;
+
+const float triangleEdgeTop = (sqrt(3) / 2);
+const float triangleEdgeCenter = (sqrt(3) / 3);
+
+const std::vector<std::string> tri_colors = {'#7DDF64', '#8EE3EF', '#BCBD8B', '#FAC748'};
+
+// draw the equilateral triangles
+void drawEquilateralTriangle(float x, float y, float scale)
+{
+    glBegin(GL_TRIANGLES);
+    glVertex2f(x, y);
+    glVertex2f((+scale), y);
+    glVertex2f((X + (scale * 0.5)), (y + (scale * triangleEdgeTop)));
+    glEnd();
+}
+
+// function to convert my hex color codes to rgb
+void hexToRGB(std::string hex, float &r, float &g, float &b)
+{
+    // format of hex '#RRGGBB'
+    r = std::stoi(hex.substr(1, 2), nullptr, 16) / 255.0f;
+    b = std::stoi(hex.substr(3, 4), nullptr, 16) / 255.0f;
+    g = std::stoi(hex.substr(5, 6), nullptr, 16) / 255.0f;
+}
+
+// function to draw the sierpinski with custom colors
+void drawTheSierpinski()
+{
+    float scale = 10.0f;
+    float x = 0.0f;
+    float y = 0.0f;
+
+    for (int i = 0; i < 3; ++i)
+    {
+        // set the new colors
+        float r, g, b;
+        hexToRGB(colors[i], r, g, b);
+        glColor3f(r, g, b);
+
+        // initialize the triangle
+        drawEquilateralTriangle(x, y, scale);
+        x += scale / 2.0f;
+        y += scale * triangleEdgeTop;
+    }
+}
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    drawTheSierpinski();
+    glFlush();
+};
+
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(400, 400);
+    glutCreateWindow("Sierpinski Triangle: Level 3");
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(-10, 10, -5, 15);
+    glutDisplayFunc(display);
+    glutMainLoop();
+    return 0;
+}
---
+++ C++/Graphics/Blender Sierpinski/sierpinski.py
@@ -0,0 +1,141 @@
+import bpy
+import math
+from mathutils import Vector
+
+# pre-calculated proportions
+triangle_edge_top = math.sqrt(3) / 2  # edge to top
+triangle_edge_center = math.sqrt(3) / 3  # edge to center
+tri_colors = [
+    '#7DDF64',
+    '#8EE3EF',
+    '#BCBD8B',
+    '#FAC748',
+    
+]
+
+
+
+# triangle function
+def create_equilateral_triangle(name, scale, origin=Vector((0, 0, 0))):
+    # shape components
+    coords = [
+        origin + Vector((0, 0, 0)),
+        origin + Vector((scale, 0, 0)),
+        origin + Vector((scale / 2, scale * triangle_edge_top, 0))
+    ]
+    edges = [
+    ]
+    faces = [
+        (0, 1, 2)
+    ]
+
+    # create new mesh and new object
+    mesh = bpy.data.meshes.new(f'{name}-mesh')
+    obj = bpy.data.objects.new(name, mesh)
+
+    # make mesh from shape components
+    mesh.from_pydata(coords, edges, faces)
+
+    # show name and update the mesh
+    mesh.update()
+
+    # link object to active collection
+    bpy.context.collection.objects.link(obj)
+
+    return obj
+
+
+def copy_cat_triangle(obj, suffix, translate):
+    # duplicate object
+    obj2 = bpy.data.objects.new(obj.name + suffix, obj.data)
+
+    # move object
+    obj2.location += translate
+
+    # link object to active collection
+    bpy.context.collection.objects.link(obj2)
+
+    return obj2
+
+def clear_extra_vertices():
+    # clean up extra vertices
+    # disable this for bpy.ops.mesh.select_more() to color entire tris (vs sides)
+    bpy.ops.object.mode_set(mode='EDIT')
+    bpy.ops.mesh.select_all(action='SELECT')
+    bpy.ops.mesh.remove_doubles(threshold=merge_threshold)
+    bpy.ops.object.mode_set(mode='OBJECT')
+
+
+def grenier_hormann_clipping(tri):
+    # defining the bounding box of the initial triangle
+    min_x = min(vert.co.x for vert in triangle.data.vertices)
+    min_y = min(vert.co.y for vert in triangle.data.vertices)
+    max_x = max(vert.co.x for vert in triangle.data.vertices)
+    max_y = max(vert.co.y for vert in triangle.data.vertices)
+    
+    # defining the vertices of the initial triangle
+    vertices = [
+        (vert.co.x, vert.co.y) for vert in triangle.data.vertices 
+    ]
+    
+    for obj in bpy.data.objects:
+        if obj != triangle and obj.type == 'MESH':
+            obj_vertices = [
+                (v.co.x, v.co.y) for v in obj.data.vertices
+            ]
+            # check any vertex of the triangle lies outside the initial triangle bounding box
+            if any((vx < min_x or vx > max_x) or (vy < min_y or vy > max_y) for vx, vy in obj_vertices):
+                # select the objects to be clipped
+                obj.select_set(True)
+    
+    # clip aka remove the selected objects                
+    bpy.ops.objects.delete()
+
+
+def main():
+    # set iterations
+    iter = 3  # 8
+    # set sizes
+    size = 10
+    unit = size / (2 ** iter)
+    merge_threshold = unit / 2
+
+    # offset 3d cursor
+    cursor = bpy.context.scene.cursor.location
+    offset = cursor + Vector((-size / 2, -size * triangle_edge_center / 2, 0))
+
+    # create base triangle
+    triangle = create_equilateral_triangle('Sierpinski Triangle', unit, offset)
+
+    # iterations
+    for i in range(iter):
+
+        # set base transform
+        base = 2 ** i
+
+        # set tri
+        triangle = bpy.data.objects['Sierpinski Triangle']
+
+        # duplicate/move the "object" in x axis
+        triangle_x = copy_cat_triangle(triangle, '-X', Vector((base * unit, 0, 0)))
+        triangle_x.select_set(state=True)
+
+        # duplicate/move the "object" on triangle direction
+        triangle_t = copy_cat_triangle(triangle, '-T', Vector((base * unit / 2, base * unit * triangle_edge_top, 0)))
+        triangle_t.select_set(state=True)
+
+        # join all objects as "object"
+        triangle.select_set(state=True)
+        bpy.context.view_layer.objects.active = triangle
+        bpy.ops.object.join()
+        
+        # apply the clipping algorithms to the triangle
+        # grenier_hormann_clipping(triangle)
+        
+        # pause for 2 seconds
+        # bpy.context.scene.frame_set(bpy.context.scene.frame_current + 20)
+        
+    clear_extra_vertices()
+    
+if __name__ == '__main__':
+    main()
