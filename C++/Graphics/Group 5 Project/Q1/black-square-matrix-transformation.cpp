723d057897e3b4321b474002786f1ea41db08323
magvtv <magutujr@gmail.com>
2024-03-06 12:25:12 +0300

    formerly did matrix operation, done basic geometric transformation

---
+++ C++/Graphics/Group 5 Project/Q1/black-square-matrix-transformation.cpp
@@ -0,0 +1,82 @@
+#include <GL/glut.h>
+
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
+const float SQUARE_SIZE = 50.0f;
+
+class OpenGLAnimation
+{
+public:
+    OpenGLAnimation()
+    {
+        angle = 0.0f;
+    }
+
+    // function to initialize the opengl
+    void init()
+    {
+        // set the background color to white
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        gluOrtho2D(0, WINDOW_WIDTH, 0, WINDOW_HEIGHT);
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+    }
+
+    // function to draw the black square
+    void drawBlackSquare()
+    {
+        // set the square color to black
+        glColor3f(0.0f, 0.0f, 0.0f);
+        glBegin(GL_POLYGON);
+        glVertex2f((-SQUARE_SIZE * 0.5), (-SQUARE_SIZE * 0.5));
+        glVertex2f((SQUARE_SIZE * 0.5), (-SQUARE_SIZE * 0.5));
+        glVertex2f((SQUARE_SIZE * 0.5), (SQUARE_SIZE * 0.5));
+        glVertex2f((-SQUARE_SIZE * 0.5), (SQUARE_SIZE * 0.5));
+        glEnd();
+    }
+
+    // function to handle the rendering
+    void display()
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glLoadIdentity();
+        glTranslatef(float(WINDOW_WIDTH / 2), float(WINDOW_HEIGHT / 2), 0);
+        glRotatef(angle, 0, 0, 1);
+        drawBlackSquare();
+        glutSwapBuffers();
+    }
+
+    // function handling the animation
+    void animate(int value)
+    {
+        angle += 1.0f;
+        if (angle > 360.0f)
+        {
+            angle -= 360.0f;
+        }
+        // request for a redrawing of the square
+        glutPostRedisplay();
+        glutTimerFunc(16, animate, 0);
+    }
+
+private:
+    float angle;
+}
+
+// global open gl animation object
+OpenGLAnimation animation;
+
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    glutCreateWindow("Black Square Animation");
+    animation.init();
+    glutDisplayFunc(display);
+    glutTimerFunc(0, animate, 0);
+    glutMainLoop();
+    return 0;
+}
