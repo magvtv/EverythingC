b228babf2f19a380d61409c118150e49fb31b37c
magvtv <magutujr@gmail.com>
2024-03-22 15:21:44 +0300

    bugs: tried to fix going to Q2

--- C++/Graphics/Group 5 Project/Q1/matrix-clockwise-square.cpp
+++ C++/Graphics/Group 5 Project/Q1/matrix-clockwise-square.cpp
@@ -1,8 +1,11 @@
 #include <GL/glut.h>
+#include <cmath>
 
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 600;
 const float SQUARE_SIZE = 50.0f;
+const float CIRCLE_RADIUS = 200.0f;
+const float ANIMATION_SPEED = 1.0f;
 
 class OpenGLAnimation
 {
@@ -40,30 +43,39 @@ public:
         glEnd();
     }
 
-    // member function to handle the rendering
-    void display()
-    {
-        glClear(GL_COLOR_BUFFER_BIT);
-        glLoadIdentity();
-        glTranslatef(float(WINDOW_WIDTH / 2), float(WINDOW_HEIGHT / 2), 0);
-        glRotatef(angle, 0, 0, 1);
-        drawBlackSquare();
-        glutSwapBuffers();
-    }
-
     // member function handling the animation
-    void animate(int value)
+    static void animate(int value)
     {
-        angle += 1.0f;
+        OpenGLAnimation *animation = static_cast<OpenGLAnimation *>(value);
+        animation->angle += 1.0f angle += ANIMATION_SPEED;
         if (angle > 360.0f)
         {
             angle -= 360.0f;
         }
-        // request for a redrawing of the square
+
+        // float x = CIRCLE_RADIUS * cos((angle * M_PI) / 180.0f);
+        // float y = CIRCLE_RADIUS * sin((angle * M_PI) / 180.0f);
+
+        // glMatrixMode(GL_MODELVIEW);
+        // glLoadIdentity();
+        // glTranslatef(x, y, 0);
         glutPostRedisplay();
-        glutTimerFunc(16, animate, 0);
+        glutTimerFunc(16, animate, reinterpret_cast<int>(animation));
     }
 };
+
+// member function to handle the rendering
+void display()
+{
+    // glClear(GL_COLOR_BUFFER_BIT);
+    // glLoadIdentity();
+    // glTranslatef(float(WINDOW_WIDTH / 2), float(WINDOW_HEIGHT / 2), 0);
+    // glRotatef(angle, 0, 0, 1);
+    // drawBlackSquare();
+    // glutSwapBuffers();
+
+    animation->display();
+}
 
 // global open gl animation object
 
@@ -76,7 +88,7 @@ int main(int argc, char **argv)
     glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
     glutCreateWindow("Black Square Animation");
     glutDisplayFunc(animation.display);
-    glutTimerFunc(0, animation.animate, 0);
+    glutTimerFunc(0, animation.animate, reinterpret_cast<int>(animation));
     glutMainLoop();
     return 0;
 }
