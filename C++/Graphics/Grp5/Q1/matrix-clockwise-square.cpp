35d459cc9af3af59fc859f25d6f84f1f9333e131
magvtv <magutujr@gmail.com>
2024-03-26 16:07:27 +0300

    fixed the whole of Q1 GADDEMMMIITTT!!!

--- C++/Graphics/Grp5/Q1/matrix-clockwise-square.cpp
+++ C++/Graphics/Grp5/Q1/matrix-clockwise-square.cpp
@@ -10,7 +10,7 @@ const float ANIMATION_SPEED = 1.0f;
 class OpenGLAnimation
 {
 private:
-    float angle;
+    static float angle;
 
 public:
     OpenGLAnimation()
@@ -31,7 +31,7 @@ public:
     }
 
     // member function to draw the black square
-    void drawBlackSquare()
+    static void drawBlackSquare()
     {
         // set the square color to black
         glColor3f(0.0f, 0.0f, 0.0f);
@@ -46,38 +46,37 @@ public:
     // member function handling the animation
     static void animate(int value)
     {
-        OpenGLAnimation *animation = static_cast<OpenGLAnimation *>(value);
-        animation->angle += 1.0f angle += ANIMATION_SPEED;
+        OpenGLAnimation *animation = reinterpret_cast<OpenGLAnimation *>(value);
+        animation->angle += 1.0f;
+        angle += ANIMATION_SPEED;
         if (angle > 360.0f)
         {
             angle -= 360.0f;
         }
 
-        // float x = CIRCLE_RADIUS * cos((angle * M_PI) / 180.0f);
-        // float y = CIRCLE_RADIUS * sin((angle * M_PI) / 180.0f);
+        float x = CIRCLE_RADIUS * cos((angle * M_PI) / 180.0f);
+        float y = CIRCLE_RADIUS * sin((angle * M_PI) / 180.0f);
 
-        // glMatrixMode(GL_MODELVIEW);
-        // glLoadIdentity();
-        // glTranslatef(x, y, 0);
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        glTranslatef(x, y, 0);
         glutPostRedisplay();
-        glutTimerFunc(16, animate, reinterpret_cast<int>(animation));
+        glutTimerFunc(16, animate, value);
+    }
+
+    // member function to handle the rendering
+    static void display()
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glLoadIdentity();
+        glTranslatef(float(WINDOW_WIDTH / 2), float(WINDOW_HEIGHT / 2), 0);
+        glRotatef(angle, 0, 0, 1);
+        OpenGLAnimation::drawBlackSquare();
+        glutSwapBuffers();
     }
 };
 
-// member function to handle the rendering
-void display()
-{
-    // glClear(GL_COLOR_BUFFER_BIT);
-    // glLoadIdentity();
-    // glTranslatef(float(WINDOW_WIDTH / 2), float(WINDOW_HEIGHT / 2), 0);
-    // glRotatef(angle, 0, 0, 1);
-    // drawBlackSquare();
-    // glutSwapBuffers();
-
-    animation->display();
-}
-
-// global open gl animation object
+float OpenGLAnimation::angle = 0.0f;
 
 int main(int argc, char **argv)
 {
@@ -87,8 +86,8 @@ int main(int argc, char **argv)
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
     glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
     glutCreateWindow("Black Square Animation");
-    glutDisplayFunc(animation.display);
-    glutTimerFunc(0, animation.animate, reinterpret_cast<int>(animation));
+    glutDisplayFunc(OpenGLAnimation::display);
+    glutTimerFunc(0, OpenGLAnimation::animate, 0);
     glutMainLoop();
     return 0;
 }
